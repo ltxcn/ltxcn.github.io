e031e080dca2265525c08e286f96eaa96fd3548e
ltxcn <2147525492@qq.com>
2024-03-02 09:21:35 +0800

    Site updated: 2024-03-02 09:21:34

--- js/custom.js
+++ js/custom.js
@@ -1,1 +1,24 @@
-function share(){let t=window.location.origin+window.location.pathname;new ClipboardJS(".share",{text:function(){return"标题："+document.title+"\n链接："+t}}),btf.snackbarShow("本页链接已复制到剪切板，快去分享吧~")}var titleTime,OriginTitile=document.title;document.addEventListener("visibilitychange",(function(){document.hidden?(document.title="w(ﾟДﾟ)w 不要走！再看看嘛！",clearTimeout(titleTime)):(document.title="♪(^∇^*)欢迎肥来！"+OriginTitile,titleTime=setTimeout((function(){document.title=OriginTitile}),2e3))}));
+// 分享本页
+function share() {
+    let url = window.location.origin + window.location.pathname
+    new ClipboardJS(".share", { text: function() { return '标题：' + document.title + '\n链接：' + url } });
+    btf.snackbarShow("本页链接已复制到剪切板，快去分享吧~")
+}
+
+//动态标题
+var OriginTitile = document.title;
+var titleTime;
+document.addEventListener("visibilitychange", function () {
+  if (document.hidden) {
+    //离开当前页面时标签显示内容
+    document.title = "w(ﾟДﾟ)w 不要走！再看看嘛！";
+    clearTimeout(titleTime);
+  } else {
+    //返回当前页面时标签显示内容
+    document.title = "♪(^∇^*)欢迎肥来！" + OriginTitile;
+    //两秒后变回正常标题
+    titleTime = setTimeout(function () {
+      document.title = OriginTitile;
+    }, 2000);
+  }
+});
